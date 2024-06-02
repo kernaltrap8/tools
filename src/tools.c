8c7ccb5eb9d5c9a249f3c4580cac3e6f202c4b17
kernaltrap8 <kernaltrap@gmail.com>
2024-06-02 15:35:09 -0500

    fix some flag handling bugs

--- src/tools.c
+++ src/tools.c
@@ -12,7 +12,9 @@
   "tools Copyright (C) 2024 kernaltrap8\nThis program is licensed under the "  \
   "GNU GPLv3.\n'r' or 'randstring'\n    prints a random string using the "     \
   "characters from the ascii table with the length of 32 chars\n'c' or "       \
-  "'countlength'\n    counts the length of a string, passed from stdin\n"
+  "'countlength'\n    counts the length of a string, passed from stdin\n'p' "  \
+  "or printreg\n    prints the value of a register (eax, ebx, ecx, edx, ebp, " \
+  "esp, eip)"
 #define VERSION "0.2"
 
 void randstring(void) {
@@ -36,25 +38,71 @@ void countlength(char *string) {
     count++;
   }
 
-  printf("%d", count);
+  printf("%d\n", count);
+}
+
+void printreg(char *r) {
+  int reg_value;
+  // store the value of each register in reg_value then print it
+  if (!strcmp(r, "eip")) {
+    void *reg_value;
+    asm("call next_instruction\nnext_instruction:\npop %0" : "=r"(reg_value));
+    printf("%p\n", reg_value);
+  } else {
+    if (!strcmp(r, "eax")) {
+      asm("movl %%eax, %0" : "=r"(reg_value));
+    }
+    if (!strcmp(r, "ebx")) {
+      asm("movl %%ebx, %0" : "=r"(reg_value));
+    }
+    if (!strcmp(r, "ecx")) {
+      asm("movl %%ecx, %0" : "=r"(reg_value));
+    }
+    if (!strcmp(r, "edx")) {
+      asm("movl %%edx, %0" : "=r"(reg_value));
+    }
+    if (!strcmp(r, "ebp")) {
+      asm("movl %%ebp, %0" : "=r"(reg_value));
+    }
+    if (!strcmp(r, "esp")) {
+      asm("movl %%esp, %0" : "=r"(reg_value));
+    }
+    printf("0x%x\n", reg_value);
+  }
 }
 
 int main(int argc, char *argv[]) {
   if (argc < 1) {
     printf("No parameters provied.\ntools Copyright (C) 2024 kernaltrap8\n"
            "This program is licensed under the GNU GPLv3.\n");
+    return 1;
   }
   if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help")) {
     printf("%s", help);
+    return 0;
   }
   if (!strcmp(argv[1], "-v") || !strcmp(argv[1], "--version")) {
-    printf("tools v%s", VERSION);
+    printf("tools v%s\n", VERSION);
+    return 0;
   }
   if (!strcmp(argv[1], "randstring") || !strcmp(argv[1], "r")) {
     randstring();
+    return 0;
   }
   if (!strcmp(argv[1], "countlength") || !strcmp(argv[1], "c")) {
+    if (argc < 3) {
+      printf("usage: tools countlength <string>\n");
+      return 1;
+    }
     countlength(argv[2]);
+    return 0;
+  }
+  if (!strcmp(argv[1], "printreg") || !strcmp(argv[1], "p")) {
+    printreg(argv[2]);
+    return 0;
+  } else {
+    printf("invalid command %s.\n%s", argv[1], help);
+    return 1;
   }
 
   return 0;
